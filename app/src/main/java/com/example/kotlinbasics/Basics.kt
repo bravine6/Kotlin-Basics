25e72504c06f745d87f1c21fc968d8028ffad15b
bravine <ropbravine@gmail.com>
2022-05-27 07:17:11 +0300

    Initial commit

---
+++ app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -0,0 +1,12 @@
+package com.example.kotlinbasics
+
+fun main(){
+
+   // var h2= "kevin"
+    //val h3 = "bryan"
+    val mystrin ="Hello World"
+    var FirstCharacter = mystrin[0]
+    var LastCharacter = mystrin[mystrin.length - 2]
+    print("Last Character " + LastCharacter)
+
+    }
